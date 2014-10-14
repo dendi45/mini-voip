6a02bce2ad3c496393d47564772fb19615907222
Evan Ram <evanram@me.com>
2014-10-14 03:08:44 -0500

    Debug information logged; Refactoring

--- src/main/java/com/evanram/voip/server/UDPServer.java
+++ src/main/java/com/evanram/voip/server/UDPServer.java
@@ -1,14 +1,15 @@
 package com.evanram.voip.server;
 
+import static com.evanram.voip.VoIPApplication.BUFFER_SIZE;
+import static com.evanram.voip.VoIPApplication.playSound;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
-import static com.evanram.voip.VoIPApplication.*;
-
 public class UDPServer extends Server
 {
-	private DatagramSocket socket;
+	private DatagramSocket serverDatagramSocket;
 	
 	public UDPServer(int port)
 	{
@@ -20,19 +21,14 @@ public class UDPServer extends Server
 	{
 		try
 		{
-			socket = new DatagramSocket(port);
+			serverDatagramSocket = new DatagramSocket(port);
+			System.out.println("Server datagram socket created");
 			
 			while(running)
 			{
 				DatagramPacket packet = new DatagramPacket(new byte[BUFFER_SIZE], BUFFER_SIZE);
-				socket.receive(packet);
-
-//				System.out.println("packet: ["
-//						+ "len=" + packet.getLength() + 
-//						", offset=" + packet.getOffset() + 
-//						", port=" + packet.getPort() + 
-//						", ip=" + packet.getAddress().getHostAddress() + "]");
-				
+				serverDatagramSocket.receive(packet);
+				//for debugging: printPacket(packet);
 				byte[] buffer = packet.getData();
 				playSound(buffer);
 			}
@@ -41,5 +37,16 @@ public class UDPServer extends Server
 		{
 			e.printStackTrace();
 		}
+	}
+	
+	@SuppressWarnings("unused")
+	private void printPacket(DatagramPacket packet)
+	{
+		//ex. 'packet: [len=16000, offset=0, port=50028, ip=127.0.0.1]'
+		System.out.println("packet: ["
+				+ "len=" + packet.getLength() + 
+				", offset=" + packet.getOffset() + 
+				", port=" + packet.getPort() + 
+				", ip=" + packet.getAddress().getHostAddress() + "]");
 	}
 }
