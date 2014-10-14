3c5a7c06993342ae75fcb1fc0349596d1da187a5
Evan Ram <evanram@me.com>
2014-10-14 03:07:54 -0500

    Added support for TCP server

--- src/main/java/com/evanram/voip/server/TCPServer.java
+++ src/main/java/com/evanram/voip/server/TCPServer.java
@@ -1,8 +1,20 @@
 package com.evanram.voip.server;
 
+import static com.evanram.voip.VoIPApplication.BUFFER_SIZE;
+import static com.evanram.voip.VoIPApplication.playSound;
+import static com.evanram.voip.VoIPApplication.tcpSocketOK;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class TCPServer extends Server
 {
+	private ServerSocket serverSocket;
+	private Socket remoteSocket;
+	private DataInputStream remoteSocketInputStream;
+	
 	public TCPServer(int port)
 	{
 		super(port);
@@ -11,6 +23,23 @@ public class TCPServer extends Server
 	@Override
 	public void enterServerLoop()
 	{
-		throw new UnsupportedOperationException("No current support for TCP server");
+		try
+		{
+			serverSocket = new ServerSocket(port);
+			remoteSocket = serverSocket.accept();
+			remoteSocketInputStream = new DataInputStream(remoteSocket.getInputStream());
+			System.out.println("TCP server accepted and found input stream for socket: " + remoteSocket.toString());
+
+			while(running && tcpSocketOK(remoteSocket))
+			{
+				byte[] buffer = new byte[BUFFER_SIZE];
+				remoteSocketInputStream.read(buffer, 0, buffer.length);
+				playSound(buffer);
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
