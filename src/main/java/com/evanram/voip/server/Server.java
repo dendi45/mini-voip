2a18f9211149367c24df7ccf7a48703fafe4084e
Evan Ram <evanram@me.com>
2014-10-15 20:44:26 -0500

    Handle non-audio packets, added stopServer method

--- src/main/java/com/evanram/voip/server/Server.java
+++ src/main/java/com/evanram/voip/server/Server.java
@@ -1,5 +1,12 @@
 package com.evanram.voip.server;
 
+import static com.evanram.voip.CallData.END_CALL;
+import static com.evanram.voip.VoIPApplication.*;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+
 public abstract class Server extends Thread
 {
 	protected final int port;
@@ -11,7 +18,8 @@ public abstract class Server extends Thread
 		this.port = port;
 	}
 	
-	public void run()
+	@Override
+	public final void run()
 	{
 		if(running)
 			throw new IllegalStateException(
@@ -23,4 +31,44 @@ public abstract class Server extends Thread
 	}
 	
 	public abstract void enterServerLoop();
+	public abstract void implementedStopServer() throws IOException;
+	
+	public void handle(byte[] bytes)
+	{
+		int length = bytes.length;
+		
+		if(length == bufferSize)
+		{
+			playSound(bytes);
+		}
+		else
+		{
+			if(Arrays.equals(bytes, END_CALL))
+			{
+				instance.endCall();
+			}
+		}
+	}
+	
+	public final void stopServer()
+	{
+		System.out.println("Stopping server");
+		running = false;
+		
+		try
+		{
+			implementedStopServer();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		System.out.println("Server stopped");
+	}
+
+	public boolean isRunning()
+	{
+		return running;
+	}
 }
