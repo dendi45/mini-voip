0b44a1e96e551aced3ffa996dbacaf305cc263de
Evan Ram <evanram@me.com>
2014-10-15 20:39:34 -0500

    sendPacket method, send END_CALL bytes on stop

--- src/main/java/com/evanram/voip/client/UDPClient.java
+++ src/main/java/com/evanram/voip/client/UDPClient.java
@@ -1,6 +1,6 @@
 package com.evanram.voip.client;
 
-import static com.evanram.voip.VoIPApplication.BUFFER_SIZE;
+import static com.evanram.voip.VoIPApplication.bufferSize;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -8,6 +8,8 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 import javax.sound.sampled.TargetDataLine;
+
+import com.evanram.voip.CallData;
 
 
 public class UDPClient extends Client
@@ -26,29 +28,31 @@ public class UDPClient extends Client
 			return;
 		
 		//TODO keep-alive packets to make sure that the other end is still alive
-		//XXX boolean keptAlive = true;
 		
-		while(running/*XXX && keptAlive*/)
+		while(running/*&& keptAlive*/)
 		{
-			//---------------------------
-			//XXX keptAlive = /*ping ping ping response response response, or a keep-alive thread*/
-			//---------------------------
-			
-			byte[] buffer = new byte[BUFFER_SIZE];
-			
-			//XXX probably invoke a thread to check keep alive
+			byte[] buffer = new byte[bufferSize];
 			targetDataLine.read(buffer, 0, buffer.length);
-			DatagramPacket packet = new DatagramPacket(buffer, buffer.length, peerIp, peerPort);
-			
-			try
-			{
-				new DatagramSocket().send(packet);
-			}
-			catch(IOException e)
-			{
-				e.printStackTrace();
-				System.err.println("Error in sending packet over UDP datagram socket");
-			}
+			sendPacket(buffer);
 		}
+	}
+
+	private void sendPacket(byte[] data)
+	{
+		try
+		{
+			new DatagramSocket().send(new DatagramPacket(data, data.length, peerIp, peerPort));
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+			System.err.println("Error in sending packet over UDP datagram socket");
+		}
+	}
+	
+	@Override
+	public void implementedStopClient() throws IOException
+	{
+		sendPacket(CallData.END_CALL);
 	}
 }
